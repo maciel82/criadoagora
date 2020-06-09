26ba7ba8e8beb9d056425e55613149f871f968b3
Maciel <macielferreira25.mf@gmail.com>
2020-06-09 13:13:38 -0300

    teste jogo_teste.dart: recebendo valor na posição 0 da listaP com verificação de ícone, bem sucedido

--- chaveirinho_flutter_v1_17_2_/lib/jogo_teste/jogo_teste.dart
+++ chaveirinho_flutter_v1_17_2_/lib/jogo_teste/jogo_teste.dart
@@ -12,16 +12,33 @@ class _JogoState extends State<Jogo> {
   String vListQuestoes;
   String vListListaP;
   List listaP = ["1", "2", "3", "4"];
+  bool chequeIsso;
+  List verificaLista = [];
 
   Icon check = Icon(Icons.check);
   Icon lock = Icon(Icons.lock);
+  Icon nonePos0 = Icon(Icons.remove_red_eye);
 
   List listaQuestoes = ["primeira", "segunda", "terceira", "quarta"];
 
-  void mudaIcon(){
+  @override
+  void initState() {
     setState(() {
-      
+      posListaP = 0;
+      Map<String, dynamic> addList = Map();
+      addList["title"] = listaP[posListaP];
+      verificaLista.add(addList);
+      addList["ok"] = false;
+      verificaLista[0]["ok"] = true;
     });
+    super.initState();
+  }
+
+  void checkList(){
+    Map<String, dynamic> addList = Map();
+    addList["title"] = listaP[posListaP];
+    verificaLista.add(addList);
+    addList["ok"] = false;
   }
   
   @override
@@ -42,27 +59,30 @@ class _JogoState extends State<Jogo> {
                     mainAxisSpacing: 5.0,
                     crossAxisSpacing: 5.0
                   ),
-                  itemCount: listaP.length,
-                  itemBuilder: (BuildContext context, int index) {
+                  itemCount: verificaLista.length,
+                  itemBuilder: (BuildContext context, int index) {                    
                     return Card(
                       child: GestureDetector(
-                        child: Container(
-                          child: check
+                        child: Stack(
+                          children: <Widget>[
+                            verificaLista[0]["ok"]? nonePos0 : lock,
+                            Container(
+                              child: Center(child: Text(verificaLista[index]["title"], style: TextStyle(fontSize: 20.0),)),
+                            )
+                          ],
                         ),
                         onTap: (){
                           //pega valor da posição da lista
                           vListListaP = listaP[index];
-                          posListaP = int.parse(vListListaP) - 1;
+                          posListaP = index; //int.parse(vListListaP) - 1;
                           vListQuestoes = listaQuestoes[posListaP];
 
-                          listaP[index] = true;
-                          
-                          print("vlist: $vListListaP");                        
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => QuestoesJogoTeste(this.vListQuestoes)));
-                          print("v: $posListaP");
+                          print(posListaP);
+                                                                            
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => QuestoesJogoTeste(this.vListQuestoes)));                            
                         },
                       ),
-                    );
+                    );                                     
                   },
                 ),
               )
