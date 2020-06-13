821170af659166068f3593dade7ee56a714a52ab
Maciel <macielferreira25.mf@gmail.com>
2020-06-13 19:27:57 -0300

    corrigido erro de variável email no app agenda de contatos

--- chaveirinho_flutter_v1_17_2_/lib/agenda_contatos/helper/contact_helper.dart
+++ chaveirinho_flutter_v1_17_2_/lib/agenda_contatos/helper/contact_helper.dart
@@ -1,8 +1,9 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+
 import 'dart:async';
 
-final String contactTable = "ContactTable";
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "emailColumn";
@@ -10,6 +11,7 @@ final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgColumn";
 
 class ContactHelper{
+
   static final ContactHelper _instance = ContactHelper.internal();
 
   factory ContactHelper() => _instance;
@@ -18,7 +20,7 @@ class ContactHelper{
 
   Database _db;
 
-  Future<Database>get db async{
+  Future<Database> get db async{
     if (_db != null) {
       return _db;
     } else {
@@ -27,49 +29,51 @@ class ContactHelper{
     }
   }
 
-  Future<Database> initDb() async{
+  Future<Database> initDb() async {
     final databasesPath = await getDatabasesPath();
-    final path = join(databasesPath, "contacts.db");
+    final path = join(databasesPath, "contactsnew.db");
 
-    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion)async{
-      await db.execute("CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)");
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)"
+      );
     });
   }
 
-  Future<Contact>saveContact(Contact contact) async{
-    Database dbContact = await _db;
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
     contact.id = await dbContact.insert(contactTable, contact.toMap());
     return contact;
   }
 
-  Future<Contact> getContact(int id)async{
-    Database dbContact = await _db;
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
     List<Map> maps = await dbContact.query(contactTable,
       columns: [idColumn, nameColumn, emailColumn, phoneColumn, imgColumn],
       where: "$idColumn = ?",
       whereArgs: [id]);
-    if (maps.length > 0) {
+    if(maps.length > 0){
       return Contact.fromMap(maps.first);
     } else {
       return null;
     }
   }
 
-  Future<int>deleteContact(int id) async{
-    Database dbContact = await _db;
+  Future<int> deleteContact(int id) async {
+    Database dbContact = await db;
     return await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
   }
 
-  Future<int>update(Contact contact) async{
-    Database dbContact = await _db;
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
     return await dbContact.update(contactTable, 
       contact.toMap(), 
       where: "$idColumn = ?",
       whereArgs: [contact.id]);
   }
   //obter todos os contatos
-  Future<List>getAllContacts() async{
-    Database dbContact = await _db;
+  Future<List> getAllContacts() async {
+    Database dbContact = await db;
     List listMap = await dbContact.rawQuery("SELECT * FROM $contactTable");
     List<Contact> listContact = List();
     for(Map m in listMap){
@@ -82,15 +86,16 @@ class ContactHelper{
     Database dbContact = await db;
     return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
   }
-
   //fecha o banco de dados
   Future close() async {
     Database dbContact = await db;
     dbContact.close();
   }
+
 }
 
 class Contact{
+
   int id;
   String name;
   String email;
@@ -107,10 +112,10 @@ class Contact{
     img = map[imgColumn];
   }
 
-  Map toMap(){
+  Map toMap() {
     Map<String, dynamic> map = {
       nameColumn: name,
-      email: email,
+      emailColumn: email,
       phoneColumn: phone,
       imgColumn: img
     };
@@ -120,7 +125,9 @@ class Contact{
     return map;
   }
 
-  String toString(){
+  @override
+  String toString() {
     return "Contact(id: $id, nome: $name, email: $email, phone: $phone, img: $img)";
   }
+
 }
