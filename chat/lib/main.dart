d42ece8cc0c16dbe67f74bcc2bd882470139b687
Maciel <macielferreira25.mf@gmail.com>
2020-07-03 18:00:38 -0300

    118. Lendo Dados no Banco: app chat

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -1,15 +1,54 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
   runApp(MyApp());
 
+  /* setando dados no firebase
+  
   Firestore.instance.collection('mensagens').document('msg1')
   .setData({
      'texto': 'olá, tudo bem?', 
      'from': 'Maciel',
      'read': false
-  });
+  });*/
+
+  /*atualizando dados no firebase
+  
+  Firestore.instance.collection('mensagens').document('msg1')
+  .updateData({
+     'texto': 'sim!', 
+  });*/
+
+  /*obtem todos os dados
+  
+  QuerySnapshot snapshot = await Firestore.instance.collection('mensagens').getDocuments();
+  snapshot.documents.forEach((element) {
+    print(element.data); //escreve o dados
+    print(element.documentID); //escreve o id do dado
+    //element.reference.updateData({'read': true}); //modifica o campo
+  });*/
+
+  /*obtem dado a partir do id
+  
+  DocumentSnapshot snapshot = await Firestore.instance.collection('mensagens').document('msg1').get();
+  print(snapshot.data);
+  */
+
+  /*ler todos os documentos em tempo real
+  
+  Firestore.instance.collection('mensagens').snapshots().listen((event) {
+    event.documents.forEach((element) {
+      print(element.data);
+    });
+  });*/
+
+  /*ler dado do documento em específico
+  
+  Firestore.instance.collection('mensagens').document('msg1').snapshots().listen((event) {
+    print(event.data);
+  });*/
+
 }
 
 class MyApp extends StatelessWidget {
