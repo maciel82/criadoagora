a47ff8b3c65085b394aea30e6188a49e6a0db203
Maciel <macielferreira25.mf@gmail.com>
2020-10-20 16:57:18 -0300

    132. Navegando entre Páginas

--- loja_virtual/lib/main.dart
+++ loja_virtual/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'home_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,8 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 155, 121),      
+      ),
+      home: HomeScreen()
     );
   }
 }
